6ad56a59e49c87ec7756f95c232142e454ea65ec
Tóth Levente <tlevente206@gmail.com>
2026-02-12 23:25:25 +0100

    kesz alap

---
+++ src/main/java/ua/com/nrgy/MainApp.java
@@ -0,0 +1,27 @@
+package ua.com.nrgy;
+
+import atlantafx.base.theme.PrimerLight;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // AtlantaFX téma beállítása (ettől lesz szép modern)
+        Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
+        stage.setTitle("Református Gyülekezeti Nyilvántartó");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/ua/com/nrgy/controller/MainController.java
+++ src/main/java/ua/com/nrgy/controller/MainController.java
@@ -1,4 +1,16 @@
 package ua.com.nrgy.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class MainController {
+
+    @FXML
+    private TableView<?> tagokTablazat; // Egyelőre üres, amíg nincs Model
+
+    @FXML
+    private void handleUjTag() {
+        System.out.println("Gomb megnyomva! Itt nyílna meg majd az ablak.");
+    }
 }
